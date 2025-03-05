ae6fe01f5a602a4a3bb4f267ec324ca9e28199d0
CoryOConnor94 <coryoconnor1994@gmail.com>
2025-03-05 17:29:51 +0000

    Simple mac changer script

--- mac_changer.py
+++ mac_changer.py
@@ -2,4 +2,6 @@
 
 import subprocess
 
-subprocess.call('ifconig', shell=True)
+subprocess.call('ifconig wlan0 down', shell=True)
+subprocess.call('ifconig wlan0 hw ether 00:11:22:33:44:66', shell=True)
+subprocess.call('ifconig wlan0 up', shell=True)
