691ba123b72c7fbcb86008d90e574a9fb61f0bf8
Cory_Oconnor <coryoconnor1994@gmail.com>
2025-03-20 09:32:33 +0000

    Initial commit

---
+++ file_inteceptor.py
@@ -0,0 +1,19 @@
+import scapy.all as scapy
+import netfilterqueue
+
+queue = netfilterqueue.NetfilterQueue()
+queue.bind(0, process_packet())   # Bind to queue created
+queue.run()
+
+def process_packet(packet):
+    scapy_packet = scapy.IP(packet.get_payload())
+    # print(scapy_packet.show())
+    if scapy_packet.haslayer(scapy.Raw):
+        if scapy_packet[scapy.TCP].dport == 80:
+            print('This is a HTTP Request')
+            print(scapy_packet.show())
+        elif scapy_packet[scapy.TCP].sport == 80:
+            print('This is a HTTP Response')
+            print(scapy_packet.show())
+
+    packet.accept()
